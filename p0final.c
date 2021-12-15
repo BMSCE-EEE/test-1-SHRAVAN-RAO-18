1ae7634fe985e694d44e80aa88f0f318032cc68e
Shravan S <88531098+SHRAVAN-RAO-18@users.noreply.github.com>
2021-12-15 03:20:30 +0000

    scope

--- p0final.c
+++ p0final.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int f()
+{
+  static int a=10;
+  a=a;
+  return a;
+}
+int main()
+{
+  int z,k;
+  z=f();
+  printf("%d\n",z);
+  k=f();
+  printf("%d\n",k);
+  return 0;
+}
--- p9final.c
+++ p9final.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 char charinput();
+char comp(char s1,char s2);
 int main()
 {
   char s1[10],s2[10],larger;
   s1[10]=charinput();
   s2[10]=charinput();
+  larger=comp(s1[10],s2[10]);
   return 0;
 }
 char charinput()
@@ -13,4 +15,24 @@ char charinput()
   printf("enter a string");
   scanf("%s",w);
   printf("%s\n",w);
+}
+char comp(char s1,char s2)
+{
+  char large;
+  int i;
+  while(s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+  {
+    i++;
+    if(s1[i]>s2[i]){
+      large=s1[i];
+    }
+    else if(s2[i]>s1[i]){
+      large=s2[i];
+    }
+    else{
+      printf("the entered strings are same");
+    }
+    break;
+  }
+  return large;
 }
