b70be4f45c323b53ce33a285680e568a7c9b85d0
Shravan S <88531098+SHRAVAN-RAO-18@users.noreply.github.com>
2021-12-16 03:21:41 +0000

    p9final

--- p9final.c
+++ p9final.c
@@ -1,32 +1,36 @@
 #include<stdio.h>
 char charinput();
-char comp(char s1,char s2);
+char comp(char s1[n],char s2[n]);
 int main()
 {
-  char s1[10],s2[10],larger;
-  s1[10]=charinput();
-  s2[10]=charinput();
-  larger=comp(s1[10],s2[10]);
+  int n=10;
+  char s1[n],s2[n],larger;
+  s1[n]=charinput();
+  s2[n]=charinput();
+  larger=comp(s1[n],s2[n]);
   return 0;
 }
 char charinput()
 {
-  char w[10];
+  int n=10;
+  char w[n];
   printf("enter a string");
   scanf("%s",w);
   printf("%s\n",w);
+  return w;
 }
-char comp(char s1,char s2)
+char comp(char s1[10],char s2[10])
 {
+  int n=10;
   char large;
   int i;
   while(s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
   {
     i++;
-    if(s1[i]>s2[i]){
+    if(s1[i]>=s2[i]){
       large=s1[i];
     }
-    else if(s2[i]>s1[i]){
+    else if(s2[i]>=s1[i]){
       large=s2[i];
     }
     else{
